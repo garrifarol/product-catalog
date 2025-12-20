9f7c311dbd4bcec83054c9e408fb5471f7de1195
Garri Farol <garri.farol@gmail.com>
2025-12-20 23:28:42 +0800

    move runtime log before querying

--- backend/src/v1/categories/categories.handlers.ts
+++ backend/src/v1/categories/categories.handlers.ts
@@ -4,8 +4,8 @@ import type { AppRouteHandler } from "../../utils/types.ts";
 import type { GetRoute } from "./categories.routes.ts";
 
 export const get: AppRouteHandler<GetRoute> = async (c) => {
-	const categories = await db.query.categories.findMany();
 	console.log("NODE_ENV:", env.NODE_ENV);
 	console.log("DATABASE_URL:", env.DATABASE_URL?.slice(0, 24) + "...");
+	const categories = await db.query.categories.findMany();
 	return c.json(categories, 200);
 };
