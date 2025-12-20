7e979578ca76ec3d339845c63e0f98f025017264
Garri Farol <garri.farol@gmail.com>
2025-12-20 23:50:47 +0800

    add try catch

--- backend/src/v1/categories/categories.handlers.ts
+++ backend/src/v1/categories/categories.handlers.ts
@@ -6,6 +6,12 @@ import type { GetRoute } from "./categories.routes.ts";
 export const get: AppRouteHandler<GetRoute> = async (c) => {
 	console.log("NODE_ENV:", env.NODE_ENV);
 	console.log("DATABASE_URL:", env.DATABASE_URL?.slice(0, 24) + "...");
-	const categories = await db.query.categories.findMany();
+	let categories;
+	try {
+		categories = await db.query.categories.findMany();
+	} catch (error) {
+		console.error("Error fetching categories:", error);
+		// return c.json({ message: "Internal Server Error" }, 500);
+	}
 	return c.json(categories, 200);
 };
